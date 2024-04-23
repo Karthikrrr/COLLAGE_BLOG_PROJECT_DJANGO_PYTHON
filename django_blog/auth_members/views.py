d8082cf7fe08a301a31ecda57692f1e6c9119452
karthikrrr <karthikrkarthi6@gmail.com>
2024-04-23 13:05:36 +0530

    edit user proile done in this branch....

--- django_blog/auth_members/views.py
+++ django_blog/auth_members/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
 from django.views import generic
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django.urls import reverse_lazy
 from auth_members.forms import SignUpForm
+
 
 class UserRegisterView(generic.CreateView):
     form_class = SignUpForm
     template_name = 'registration/register.html'
     success_url = reverse_lazy('login')
+
+class UserUpdateProfile(generic.CreateView):
+    form_class = UserChangeForm
+    template_name = 'registration/edit_profile.html'
+    success_url = reverse_lazy('home')
